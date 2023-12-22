41d2bc1bdd55e77395a573d410825f01195b5143
phichayut kotsuno <pitchayut.lek@gmail.com>
2023-12-22 20:51:07 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: 'https://api.example.com/:path*',
-  //     },
-  //   ]
-  // },
+  output: 'export',
   reactStrictMode: true,
 }
 
